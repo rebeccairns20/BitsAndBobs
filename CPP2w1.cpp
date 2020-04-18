cb63a0a31665362a61128ffcb2353927c8218e8e
rebeccairns20 <59659375+rebeccairns20@users.noreply.github.com>
2020-04-18 19:59:02 -0400

    Create CPP2w1.cpp

---
+++ CPP2w1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+  // random number generator
+  // 2 arrays
+  // pass one new number into each array for 26cards in each array
+  //ask user 1 for name and save
+  //ask user 2 for name and save
+  //While loop escape winner=one player has no cards
+  //counter for each user that counts their cards instantiate with 26
+  //inside gamePlay loop: for Loop counting cards
+  //inside gamePlay loop: inside for Loop counting cards: prompt player to press enter to lay down cards
+  //inside gamePlay loop: inside for Loop counting cards: if, else if, and else to compare whos card is higher
+  //inside gamePlay loop: inside for Loop counting cards: inside if user 1 is higher: user1 gets both cards added to the end of array and card count is added to by 1
+  //inside gamePlay loop: inside for Loop counting cards: inside if user 1 is higher: user2 loses card from array
+  //inside gamePlay loop: inside for Loop counting cards: inside if user 2 is higher: user2 gets both cards added to the end of array and card count is added to by 1
+  //inside gamePlay loop: inside for Loop counting cards: inside if user 2 is higher: user1 loses card from array
+  //inside gamePlay loop: insode for loop counting cards: inside if user 1 == user 2: while loop escape unequal cards prompt players to press enter to lay down one hiden card each and 1 visible card printed
+  //once all loops terminate print winner statement and shame looser
+
+
+
+  //example:
+  /*
+      for count<- 0 to 4:
+      Print"Text Here:
+      score<-Userinput
+      total<-total+score
+      NEXT
+      A<-10
+      B<-20
+  */
+return 0;
+}
