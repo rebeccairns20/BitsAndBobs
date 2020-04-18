56bca8085b4064e7f58d2e5c1b255bb180777767
rebeccairns20 <59659375+rebeccairns20@users.noreply.github.com>
2020-04-18 19:58:57 -0400

    Create cpp3_1_7_1.cpp

---
+++ cpp3_1_7_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//your code
+
+int main()
+{
+	vector<double> values =
+		{ 2.1, 3.14, 2.2, 1.1, 2.11, 3.2, 3.6, 2.5, 2.6, 4.11, 4.12, 3.11, 3.73, 4.83};
+	unsigned start_value;
+	cin >> start_value;
+	unsigned stop_value;
+	cin >> stop_value;
+	//your code
+	return 0;
+}
